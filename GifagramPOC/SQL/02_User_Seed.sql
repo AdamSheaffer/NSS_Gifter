353770e8d674466143db6ec9fc3f46f330ec08c8
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2020-06-24 09:15:54 -0500

    add user seed data

---
+++ GifagramPOC/SQL/02_User_Seed.sql
@@ -0,0 +1,5 @@
+﻿USE GifagramPOC
+GO
+
+INSERT INTO UserProfile ([Name], Email, ImageUrl, Bio) 
+VALUES ('Gifram Alpha', 'gifmasterflex@hotmal.com', 'https://s.gravatar.com/avatar/fdb56c6f0d806dbc42eb44a12a0d9282?s=80', NULL)
